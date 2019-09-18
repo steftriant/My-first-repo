569e9779df8c2643b60efda2646081930ac67f9b
steftriant <35741280+steftriant@users.noreply.github.com>
2019-09-18 18:08:36 +0300

    Add files via upload

---
+++ ElectronicEquipmentCompetitionsApplication.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.myproject.electronic_equipment_competitions;
+
+import org.camunda.bpm.application.ProcessApplication; 
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Electronic Equipment Competitions App")
+public class ElectronicEquipmentCompetitionsApplication extends ServletProcessApplication {
+  // empty implementation
+}
