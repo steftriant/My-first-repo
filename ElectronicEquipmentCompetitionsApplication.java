f5229282596e4ecbd18f3e131743ea7c94dcbf4c
steftriant <35741280+steftriant@users.noreply.github.com>
2020-04-02 12:10:15 +0300

    Delete ElectronicEquipmentCompetitionsApplication.java

--- ElectronicEquipmentCompetitionsApplication.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.myproject.electronic_equipment_competitions;
-
-import org.camunda.bpm.application.ProcessApplication; 
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Electronic Equipment Competitions App")
-public class ElectronicEquipmentCompetitionsApplication extends ServletProcessApplication {
-  // empty implementation
-}
